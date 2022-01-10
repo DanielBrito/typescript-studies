091ab615cae3f95127923624c0dea0fc54114276
danielbrito <daniell-henrique@hotmail.com>
2022-01-10 20:51:39 -0300

    Exercise

---
+++ JSON Types/README.md
@@ -0,0 +1,3 @@
+# JSON Types
+
+Exercise from the [course](https://www.typescript-training.com/course/fundamentals-v3) given by Mike North at Frontend Masters.
---
+++ JSON Types/exercise/src/index.ts
@@ -0,0 +1,27 @@
+type JSONPrimitive = string | number | boolean | null;
+type JSONObject = { [k: string]: JSONValue }; // index signature
+type JSONArray = JSONValue[];
+type JSONValue = JSONArray | JSONObject | JSONPrimitive;
+
+function isJSON(arg: JSONValue) {}
+
+// POSITIVE test cases (must pass)
+isJSON("hello");
+isJSON([4, 8, 15, 16, 23, 42]);
+isJSON({ greeting: "hello" });
+isJSON(false);
+isJSON(true);
+isJSON(null);
+isJSON({ a: { b: [2, 3, "foo"] } });
+
+// NEGATIVE test cases (must fail)
+// @ts-expect-error
+isJSON(() => "");
+// @ts-expect-error
+isJSON(class {});
+// @ts-expect-error
+isJSON(undefined);
+// @ts-expect-error
+isJSON(new BigInt(143));
+// @ts-expect-error
+isJSON(isJSON);
