cda20ad4dfca912690e157e6ad3de665624fa73f
danielbrito <daniell-henrique@hotmail.com>
2022-09-26 21:15:11 -0300

    Introduction

---
+++ FullStack TypeScript/README.md
@@ -0,0 +1,12 @@
+# FullStack TypeScript
+
+_TypeScript offers a delightful authoring experience, but all bets are off once you make an API call and receive an `any` response. We'll build a Twitter App with React and generate TypeScript code based on a GraphQL schema. Combine TypeScript and GraphQL's type systems to create full-stack, fully-type-checked codebases (even the data layer!), making the entire codebase shockingly easy to evolve, refactor and maintain._
+
+## Reference
+
+- [FullStack TypeScript](https://frontendmasters.com/courses/fullstack-typescript/) (Frontend Masters)
+- [Course Website](https://www.typescript-training.com/course/full-stack-typescript)
+
+## Preview
+
+TODO
---
+++ FullStack TypeScript/full-stack-ts/server/src/index.ts
@@ -0,0 +1,46 @@
+import * as chalk from 'chalk';
+import * as express from 'express';
+import { createApolloServer } from './apollo-server';
+import { createServer } from 'http';
+import { watchClientBuild } from './build-client';
+import { DB_FILE_PATH, PORT, STATIC_ROOT_FOLDER_PATH } from './constants';
+import Db from './db';
+import { seedDb } from './seed';
+
+const app = express();
+
+async function main() {
+  console.log(
+    [
+      chalk.bgBlueBright.white.bold(' Building UI and serving on '),
+      chalk.bgWhite.black('\thttp://localhost:1234\t\t'),
+    ].join(' ')
+  );
+  watchClientBuild();
+  const db = new Db(DB_FILE_PATH);
+  await db.initDefaults();
+  await seedDb(db);
+
+  app.use('/static', express.static(STATIC_ROOT_FOLDER_PATH));
+
+  const httpServer = createServer(app);
+  const apolloServer = await createApolloServer(db, httpServer, app);
+
+  await new Promise<void>((resolve) =>
+    app.listen(PORT, () => {
+      console.log(
+        [
+          chalk.bgMagentaBright.black.bold(' GraphQL API listening on   '),
+          chalk.bgWhite.black(
+            `\thttp://localhost:${PORT}${apolloServer.graphqlPath}\t`
+          ),
+        ].join(' ')
+      );
+      resolve();
+    })
+  );
+}
+
+main().catch((err) => {
+  console.error(err);
+});
