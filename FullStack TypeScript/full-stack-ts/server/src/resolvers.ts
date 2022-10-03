0d88847bda890ce6b8dede740ab6d355ad84117f
danielbrito <daniell-henrique@hotmail.com>
2022-10-03 20:50:37 -0300

    Add resolver

---
+++ FullStack TypeScript/full-stack-ts/server/src/resolvers.ts
@@ -0,0 +1,13 @@
+import Query from './resolvers/Query';
+import { Resolvers } from './resolvers-types.generated';
+import Db from './db';
+
+export interface TwitterResolverContext {
+  db: Db;
+}
+
+const resolvers: Resolvers<TwitterResolverContext> = {
+  Query,
+};
+
+export default resolvers;
